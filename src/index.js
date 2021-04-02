ce4a236e2cdae6aa0bf54c6891818e1af49bdc91
leestestaccount <rlmarsh85@gmail.com>
2021-04-01 21:46:13 -0700

    Changed board to render dynamically using a loop

--- src/index.js
+++ src/index.js
@@ -22,26 +22,22 @@ function Square(props){
     }
   
     render() {
-
+      
+      var i,j,k = 0;
+      const rows = [];
+      for(i = 0; i < 3; i++){
+        const cols = []
+        for(j = 0; j < 3; j++){
+          cols.push(this.renderSquare(k++));
+        }
+        rows.push(<div className="board-row">{cols}</div>);
+      }
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {rows}
         </div>
-      );
+      )
+
     }
   }
   
